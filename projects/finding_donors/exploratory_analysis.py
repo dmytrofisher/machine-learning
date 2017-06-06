675e0dbaf022b330fe25d353901116469eec0e31
Dmytro Slobodianyk <dmytrofisher@gmail.com>
2017-06-06 23:55:59 +0300

    Fixed according to project review

--- projects/finding_donors/exploratory_analysis.py
+++ projects/finding_donors/exploratory_analysis.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.metrics import confusion_matrix
 
 def income_by_features(data, features):    
     fig, axis = plt.subplots(ncols = len(features), figsize = (15, 5))
@@ -13,6 +14,20 @@ def income_by_features(data, features):
             axis[i].set_title("'{}' Feature Distribution".format(feature), fontsize = 14)            
         axis[i].set_title("'{}' Feature Distribution".format(feature), fontsize = 14)
         axis[i].set_ylabel("")
+     
+    fig.tight_layout()
+    fig.show()
+
+
+def confusion_matrices(X, y, classifiers):
+    fig, axis = plt.subplots(ncols = len(classifiers), figsize = (15, 5))
+    
+    for i, model in enumerate(classifiers):
+        cm = confusion_matrix(y.values, model.predict(X))        
+        sns.heatmap(cm, annot=True, cmap='Blues', xticklabels=['no', 'yes'], yticklabels=['no', 'yes'], fmt="d", ax = axis[i])
+        axis[i].set_ylabel('True label')
+        axis[i].set_xlabel('Predicted label')
+        axis[i].set_title('Confusion matrix for: {}'.format(model.__class__.__name__));
      
     fig.tight_layout()
     fig.show()
